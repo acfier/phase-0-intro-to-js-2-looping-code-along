ad840518ad9bd1bff068b8398efa1dcf7fb8513c
acfier <acfier@gmail.com>
2022-03-24 14:42:10 -0400

    commit tree cc83928a50c5dcd3433a1fa39166f5496079c14d

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log (`I'm ${age} years old.  Happy Birthday to me!`);
+// }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//         // debugger;
+//     }
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const newArr = [];
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+        newArr.push (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        debugger;
+    }
+    return newArr;
+}
+
+function countDown(num) {
+    while (num >= 0){
+        console.log(num);
+        num --;
+    }
+}
+countDown(10)
